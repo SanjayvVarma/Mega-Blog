5f4e5986af3bf03a017ba8a9ad8069f5495d2003
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-05-23 09:27:26 +0530

    feat: add subscribeRouter to handle subscription routes and fix import formatting

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,13 +28,15 @@ app.get('/', (req, res) => {
 import userRouter from "./routes/user.routes.js";
 import blogRouter from "./routes/blog.routes.js";
 import reviewRouter from "./routes/review.routes.js";
-import commentRouter from "./routes/comment.routes.js"
+import commentRouter from "./routes/comment.routes.js";
+import subscribeRouter from "./routes/subscribe.routes.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/blog", blogRouter);
 app.use("/api/v1/review", reviewRouter);
-app.use("/api/v1/comment", commentRouter)
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/subscribe", subscribeRouter);
 
 app.use((req, res) => {
     return res.status(404).json({
