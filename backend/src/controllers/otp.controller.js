204134c6da2893fa15f64c44825679bdc1ba055d
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-06-03 11:02:35 +0530

    refactor: rename OTP handling functions and update routes for clarity

--- backend/src/controllers/otp.controller.js
+++ backend/src/controllers/otp.controller.js
@@ -1,12 +1,11 @@
 import OTP from "../models/OTP.model.js";
-import otpGenerator from "otp-generator";
 import User from "../models/user.model.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
-import { sendVerificationCode } from "../utils/email.js";
+import generateAndSendOtp from "../utils/generateAndSendOtp.js";
 
-const sendOtp = asyncHandler(async (req, res) => {
+const sendOtpForRegister = asyncHandler(async (req, res) => {
     const { email } = req.body;
 
     if (!email) {
@@ -19,26 +18,32 @@ const sendOtp = asyncHandler(async (req, res) => {
         throw new ApiError(409, "User with this email already exists");
     }
 
-    const otp = otpGenerator.generate(6, {
-        digits: true,
-        upperCaseAlphabets: false,
-        lowerCaseAlphabets: false,
-        specialChars: false,
-    });
-    const otpExpiry = Date.now() + 5 * 60 * 1000;
-
-    await OTP.findOneAndUpdate(
-        { email },
-        { otp, expiresAt: otpExpiry, isVerify: false },
-        { upsert: true, new: true }
-    );
-
-    await sendVerificationCode(email, otp);
+    await generateAndSendOtp(email)
 
     return res.status(200).json(
         new ApiResponse(200, null, true, "OTP sent successfully")
     );
 
+});
+
+const sendOtpForResetPassword = asyncHandler(async (req, res) => {
+    const { email } = req.body;
+
+    if (!email) {
+        throw new ApiError(400, "email is required")
+    }
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new ApiError(404, 'You are not registered with us');
+    }
+
+    await generateAndSendOtp(email)
+
+    return res.status(200).json(
+        new ApiResponse(200, null, true, "OTP sent successfully")
+    );
 });
 
 const verifyOtp = asyncHandler(async (req, res) => {
@@ -67,4 +72,4 @@ const verifyOtp = asyncHandler(async (req, res) => {
     );
 });
 
-export { sendOtp, verifyOtp };
+export { sendOtpForRegister, sendOtpForResetPassword, verifyOtp };
--- backend/src/routes/otp.routes.js
+++ backend/src/routes/otp.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { sendOtp, verifyOtp } from '../controllers/otp.controller.js';
+import { sendOtpForRegister, sendOtpForResetPassword, verifyOtp } from '../controllers/otp.controller.js';
 
 const router = Router();
 
-router.post('/send-otp', sendOtp)
+router.post('/send-otp-register', sendOtpForRegister)
+router.post('/send-otp-reset', sendOtpForResetPassword)
 router.post('/verify-otp', verifyOtp)
 
 export default router;
