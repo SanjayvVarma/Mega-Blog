12607c2861b0bbae48b75c6de91d3274bce1d4eb
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-06-13 09:23:53 +0530

    fix: update role verification to 'SuparAdmin' for message and subscription routes

--- backend/src/routes/subscribe.routes.js
+++ backend/src/routes/subscribe.routes.js
@@ -6,7 +6,7 @@ const router = Router()
 
 router.post("/", createSubscription)
 router.get("/verify", verifySubscription);
-router.get("/view-subscriber", verifyJWT, verifyRole("Admin"), viewSubscribe);
-router.delete("/delete-sub/:id", verifyJWT, verifyRole("Admin"), deleteSubscribe)
+router.get("/view-subscriber", verifyJWT, verifyRole("SuparAdmin"), viewSubscribe);
+router.delete("/delete-sub/:id", verifyJWT, verifyRole("SuparAdmin"), deleteSubscribe)
 
 export default router;
