b525f0d6eb6bd8979993efc4583fe33cdcfca20a
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-06-15 09:27:39 +0530

    refactor: update user role enum to replace 'Admin' with 'Author'

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -57,7 +57,7 @@ const userSchema = new mongoose.Schema(
         role: {
             type: String,
             required: true,
-            enum: ["Reader", "Admin"]
+            enum: ["Reader", "Author"]
         },
 
         avatar: {
