c2386993ac4180d0c3027873433d0de264dbb0d0
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-06-19 08:44:00 +0530

    refactor: remove redundant class from VerifySubscribe component's heading

--- frontend/src/components/footer/VerifySubscribe.jsx
+++ frontend/src/components/footer/VerifySubscribe.jsx
@@ -46,7 +46,7 @@ const VerifySubscribe = () => {
         <div className="bg-gradient-to-br from-[#2b2b7f] via-[#1b1b32] to-[#591091] min-h-screen flex flex-col items-center px-4">
 
             <div className="text-white shadow-md py-2 px-10 flex justify-center gap-10 z-10 relative border-b border-blue-600">
-                <p className="relative flex items-center h-11 rounded-full text-lg font-extrabold bg-gradient-to-r from-blue-900 via-cyan-600 to-blue-900 borderAnimation border-2 font-semibold tracking-wide font-serif">
+                <p className="relative flex items-center h-11 rounded-full text-lg font-extrabold bg-gradient-to-r from-blue-900 via-cyan-600 to-blue-900 borderAnimation border-2 tracking-wide font-serif">
                     <span className="relative z-10 left-7 bottom-3 text-cyan-300 drop-shadow-md">SK</span>
                     <span className="absolute left-1/2 transform -translate-x-1/2 text-4xl heart-animate">💞</span>
                     <span className="relative z-10 text-blue-200 right-3 top-2 drop-shadow-md" >BLOG</span>
