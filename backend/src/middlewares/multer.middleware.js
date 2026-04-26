da04a70753339086430524e603bd92a21d68ab12
SanjayvVarma <spverma.sanjay56056@gmail.com>
2026-04-26 10:26:59 +0530

    fix: increase file size limit for uploads to enhance flexibility

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -22,7 +22,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
     storage,
     fileFilter,
-    limits: { fileSize: 5 * 1024 * 1024 }
+    limits: { fileSize: 10 * 1024 * 1024 }
 })
 
 export default upload;
